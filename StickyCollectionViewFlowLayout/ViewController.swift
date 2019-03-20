402325385541bd1e0eb3e10ca4b76b91013491aa
王杰 <wangjie51@meituan.com>
2019-03-20 11:26:38 +0800

    Initial Commit

---
+++ StickyCollectionViewFlowLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StickyCollectionViewFlowLayout
+//
+//  Created by 王杰 on 2019/3/20.
+//  Copyright © 2019 com.jerry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
